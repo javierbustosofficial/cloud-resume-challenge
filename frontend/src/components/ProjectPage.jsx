554b19e3b66edd29cebc87f3f5880ce8db565009
javierbustosofficial <javierbustosofficial@gmail.com>
2026-01-06 16:28:31 +0000

    Finished implementing projects and projects page. Will need to come back and edit copy along with thumbnail

--- frontend/src/components/ProjectPage.jsx
+++ frontend/src/components/ProjectPage.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import "../assets/stylesheets/projects.css";
+import "../assets/stylesheets/projects.css"
 import Header from "./Header"
 import projectsData from '../data/projectsData'
+import { useParams } from "react-router-dom"
+import { ChevronLeft } from 'lucide-react'
+import { Link } from "react-router-dom";
 
 export default function ProjectPage() {
+  const { handle } = useParams();
+
+  const project = projectsData.find(p => p.handle === handle);
   return (
     <>
       <Header></Header>
-      project show page
+      <div className="project_page">
+        <Link className="button" to={`/projects`}>
+          <ChevronLeft />
+          Back To All Projects
+        </Link>
+        <h1>{project.name}</h1>
+        <p>{project.description}</p>
+      </div>
     </>
   )
 }
